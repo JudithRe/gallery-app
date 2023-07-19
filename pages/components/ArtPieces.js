10611b7509bf7e71ade6f6d405d4ef0ff41d08c4
Ioana-Cristina Socaciu <dramaticgarlic@gmail.com>
2023-07-19 13:01:23 +0200

    render ArtPiece component as per User Story1

--- pages/components/ArtPieces.js
+++ pages/components/ArtPieces.js
@@ -1,7 +1,25 @@
+import Image from "next/image";
+
 export default function ArtPieces({ data }) {
   console.log(data);
   if (data) {
-    return <h1>Data is here</h1>;
+    return (
+      <ul>
+        {data.map(({ slug, name, artist, imageSource }) => (
+          <li key={slug}>
+            <Image
+              src={imageSource}
+              alt={`${name} by ${artist}`}
+              width={500}
+              height={500}
+            ></Image>
+            <h2>
+              {name}: {artist}
+            </h2>
+          </li>
+        ))}
+      </ul>
+    );
   }
-  return <h1>Hello</h1>;
+  return <h1>Loadin...</h1>;
 }
